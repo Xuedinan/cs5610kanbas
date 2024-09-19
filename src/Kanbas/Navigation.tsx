033e939c72043eb14d153433599d652cb39d2fe6
DinanG <gaoxuedinan@gmail.com>
2024-09-18 22:48:44 -0700

    push to a1

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -11,6 +11,6 @@ export default function KanbasNavigation() {
       <Link to="/Kanbas/Calendar" id="wd-calendar-link">Calendar</Link><br/>
       <Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/>
       <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
-      <a href="https://github.com/Xuedinan/cs5610kanbas.git" id="wd-github" target="_blank">Xuedinan Gao GitHub</a><br/>
+      <a href="https://github.com/Xuedinan/cs5610kanbas.git" id="wd-github" target="_blank">Xuedinan Gao's GitHub</a><br/>
     </div>
 );}
