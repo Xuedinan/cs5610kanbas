f93025ef2b085db110b3498f9e0efc08026fe9bd
DinanG <gaoxuedinan@gmail.com>
2024-09-18 23:06:03 -0700

    last a1 commit

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function KanbasNavigation() {
   return (
     <div id="wd-kanbas-navigation">
+      <h4>Xuedinan Gao / A1</h4>
       <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
       <Link to="/Kanbas/Account" id="wd-account-link">Account</Link><br/>
       <Link to="/Kanbas/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
@@ -12,5 +13,6 @@ export default function KanbasNavigation() {
       <Link to="/Kanbas/Inbox" id="wd-inbox-link">Inbox</Link><br/>
       <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
       <a href="https://github.com/Xuedinan/cs5610kanbas.git" id="wd-github" target="_blank">Xuedinan Gao's GitHub</a><br/>
+      
     </div>
 );}
