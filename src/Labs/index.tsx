48c60f9c6533df53c1e08755d791872df63f9ad6
DinanG <gaoxuedinan@gmail.com>
2024-10-01 20:03:54 -0700

    update final

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -10,6 +10,8 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h3>Student: Xuedinan Gao</h3>
+      <h3>Course: CS5200 2024 Fall</h3>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab" />} />
