e7b29fde96b764d344c4f24a6437ac1445b2132f
DinanG <gaoxuedinan@gmail.com>
2024-10-06 21:46:48 -0700

    a3 first commit

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import VariablesAndConstants from './VariablesAndConstants';
+import VariableTypes from './VariableTypes';
+import BooleanVariables from './BooleanVariables';
+import IfElse from './IfElse';
+import TernaryOperator from './TernaryOperator';
+import ConditionalOutputInline from './ConditionalOutputInline';
+import ConditionalOutputIfElse from './ConditionalOutputIfElse';
+import LegacyFunctions from './LegacyFunctions';
+import ArrowFunctions from './ArrowFunctions';
+import ImpliedReturn from './ImpliedReturn';
+import TemplateLiterals from './TemplateLiterals';
+import SimpleArrays from './SimpleArrays';
+import ArrayIndexAndLength from './ArrayIndexAndLength';
+import AddingAndRemovingToFromArrays from './AddingAndRemovingToFromArrays';
+import ForLoops from './ForLoops';
+import MapFunction from './MapFunction';
+import FindFunction from './FindFunction';
+import FindIndex from './FindIndex';
+import FilterFunction from './FilterFunction';
 
 export default function Lab3() {
   return (
-    <div>
-      <h2>Lab 3</h2>
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
     </div>
   );
 }
