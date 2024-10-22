5edc79362b69df99fa32f8436258920e168419e9
DinanG <gaoxuedinan@gmail.com>
2024-10-22 13:18:25 -0700

    update a4 lab

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,10 +4,15 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+
 
 export default function Labs() {
   return (
+    <Provider store={store}>
     <div>
       <h3>Labs</h3>
       <h5>Student: Xuedinan Gao</h5>
@@ -18,7 +23,9 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
+        <Route path="Lab4/*" element={<Lab4 />} />
       </Routes>
     </div>
+    </Provider>
   );
 }
