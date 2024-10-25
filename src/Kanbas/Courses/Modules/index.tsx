7584f7ec8ddb434650793c9e666dd46895dd7217
DinanG <gaoxuedinan@gmail.com>
2024-10-24 20:44:51 -0700

    update until account

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -12,28 +12,21 @@ import { IoIosArrowDown, IoIosGitNetwork } from 'react-icons/io';
 import { GrNotes } from 'react-icons/gr';
 import { GoRocket } from 'react-icons/go';
 import { useParams } from "react-router";
-import * as db from "../../Database";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
+
 
 
 export default function Modules() {
   const { cid } = useParams();
-  const [modules, setModules] = useState<any[]>(db.modules);
-
   const [moduleName, setModuleName] = useState("");
-  const addModule = () => {
-    setModules([ ...modules, { _id: new Date().getTime().toString(),
-                                     name: moduleName, course: cid, lessons: [] } ]);
-    setModuleName("");
-  };
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
-  const editModule = (moduleId: string) => {
-    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
-  };
-  const updateModule = (module: any) => {
-    setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
+  
+  const test = useSelector((state: any) => {
+    return state.modulesReducer;
+  });
+
+  const modules = test.modules;
+  const dispatch = useDispatch();
 
 
   const [isLeftMenuVisible, setLeftMenuVisible] = useState(false);
@@ -166,7 +159,11 @@ export default function Modules() {
 
 
       <div>
-        <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule} /><br /><br /><br /><br />
+      <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }} /> <br /><br /><br /><br />
         
         <ul id="wd-modules" className="list-group rounded-0 d-flex flex-grow d-md-block d-lg-block">
         {modules
@@ -178,22 +175,27 @@ export default function Modules() {
               <BsGripVertical className="me-2 fs-3" />
 
               {!module.editing && module.name}
-              
               { module.editing && (
-        <input className="form-control w-50 d-inline-block"
-               onChange={(e) => updateModule({ ...module, name: e.target.value })}
+            <input className="form-control w-50 d-inline-block"
+               onChange={(e) => 
+                dispatch(
+                updateModule({ ...module, name: e.target.value })
+              )
+            }
                onKeyDown={(e) => {
                  if (e.key === "Enter") {
-                   updateModule({ ...module, editing: false });
+                   dispatch(updateModule({ ...module, editing: false }));
                  }
                }}
                defaultValue={module.name}/>)}
 
-              <ModuleControlButtons 
-                moduleId={module._id}
-                deleteModule={deleteModule}
-                editModule={editModule}/>
+              <ModuleControlButtons moduleId={module._id}
+                  deleteModule={(moduleId) => {
+                    dispatch(deleteModule(moduleId));
+                  }}
+                  editModule={(moduleId) => dispatch(editModule(moduleId))} />
             </div>
+
             {module.lessons && (
             <ul className="wd-lessons list-group rounded-0">
               {module.lessons.map((lesson: any) => (
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,7 +7,8 @@ import Courses from "./Courses";
 import "./styles.css";
 import * as db from "./Database";
 import { useState } from "react";
-
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function Kanbas() {
 
@@ -16,6 +17,7 @@ export default function Kanbas() {
     _id: "1234", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
   });
+  
   const addNewCourse = () => {
     setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
   };
@@ -35,32 +37,28 @@ export default function Kanbas() {
   };
 
   return (
+    <Provider store={store}>
     <div id="wd-kanbas">
             <KanbasNavigation />
             <div className="wd-main-content-offset p-3">
             <Routes>
               <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
-              <Route path="/Dashboard" element={            <Dashboard
+              <Route path="/Dashboard" element={<Dashboard
               courses={courses}
               course={course}
               setCourse={setCourse}
               addNewCourse={addNewCourse}
               deleteCourse={deleteCourse}
               updateCourse={updateCourse}/>
-          } 
-/>
+              } 
+          />
               <Route path="/Courses/:cid/*" element={<Courses courses={courses} />} />
               <Route path="/Calendar" element={<h1>Calendar</h1>} />
               <Route path="/Inbox" element={<h1>Inbox</h1>} />
             </Routes>
             </div>
     </div>
+    </Provider>
 );}
-
-
-
-
-
-
 
