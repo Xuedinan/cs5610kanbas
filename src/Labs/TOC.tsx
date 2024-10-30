62ce4644e5f69f9a1fe015ef7eb72315a5a773c1
DinanG <gaoxuedinan@gmail.com>
2024-10-29 22:53:33 -0700

    a4 redux

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -21,7 +21,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+        <a id="wd-k" href="https://github.com/Xuedinan" className="nav-link">
           My GitHub
         </a>
       </li>
