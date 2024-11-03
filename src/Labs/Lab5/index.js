20c1039a2af9ccbf1173288c2c794ceb2e468ea3
DinanG <gaoxuedinan@gmail.com>
2024-11-03 13:47:03 -0800

    next is 3.5.7

--- src/Labs/Lab5/index.js
+++ src/Labs/Lab5/index.js
@@ -2,6 +2,10 @@ import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
@@ -10,15 +14,19 @@ export default function Lab5() {
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a href={`${REMOTE_SERVER}/lab5/welcome `}      
-           className="list-group-item">
-           Welcome
+        <a href={`${REMOTE_SERVER}/lab5/welcome `} className="list-group-item">
+          Welcome
         </a>
-      </div><hr/>
-        <EnvironmentVariables />
-        <PathParameters />
-        <QueryParameters />
-        <WorkingWithObjects />
+      </div>
+      <hr />
+      <EnvironmentVariables />
+      <PathParameters />
+      <QueryParameters />
+      <WorkingWithObjects />
+      <WorkingWithArrays />
+      <HttpClient />
+      <WorkingWithObjectsAsynchronously />
+      <WorkingWithArraysAsynchronously />
     </div>
   );
 }
